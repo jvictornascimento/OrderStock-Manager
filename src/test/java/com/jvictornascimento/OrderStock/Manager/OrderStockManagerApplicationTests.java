228cfe13c98f97c8b8f508136efaabb96d1d348d
victojoao <victojoao789@gmail.com>
2024-12-04 12:43:52 -0300

    started the project with API gateway

---
+++ src/test/java/com/jvictornascimento/OrderStock/Manager/OrderStockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.OrderStock.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderStockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
